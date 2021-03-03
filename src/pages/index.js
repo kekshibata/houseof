cd3ea0cd0644477a05ba2d86fda771abb065dbff
Koki Shibata <>
2021-03-03 21:02:06 +0900

    #

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,34 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import {graphql, Link} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <h1>Hello World</h1>
+    <ul>
+      {data.allMicrocmsBlog.edges.map(({node})=>(
+        <li key={node.blogId}>
+          <Link to={`/blog/${node.blogId}`}>{node.title}</Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query{
+    allMicrocmsBlog{
+      edges {
+        node {
+          blogId
+          title
+        }
+      }
+    }
+  }
+`
