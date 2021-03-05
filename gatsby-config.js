4dd0c4c6c4320bf7f540a5b272d39601b4724e09
高松 多聞 <takamatsutamon@takamatsu-tabun-no-iMac.local>
2021-03-05 14:57:33 +0900

    changed siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Gun Shooting',
+    description: 'ハウスオブザデッドの攻略情報',
+    author: 'koki shibata',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
